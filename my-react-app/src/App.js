c16221e1bc3bce7b8324245edf533d2eae8852ae
lsbaranda <lsbaranda@student.apc.edu.ph>
2025-05-29 03:00:42 +0000

    updated maessage

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Elo World....
+          Elo WEBPROG AFD241 World....
         </p>
         <a
           className="App-link"
